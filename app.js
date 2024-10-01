c61f93b7c7e075c1580cd4a9f021bbc8d025ff71
mayowasamuel <fpasamuelmayowa51@gmail.com>
2024-10-01 11:16:07 +0100

    new socur

--- app.js
+++ app.js
@@ -4,10 +4,64 @@
 /*
 
 Create the element in the html page 
-
-
-
-
-
 */
 
+// NESTED IF STATMENT , 
+
+// .LOOPS 
+
+//.................... Loops offer a quick and easy way to do something repeatedly .....................
+// for statement  ---- use this 
+// do...while statement
+// while statement use this 
+// labeled statement
+// break statement  use this sometimes 
+// continue statement use this sometimes 
+// for...in statement  use this sometimes 
+// for...of statement   use this sometimes 
+
+// Know loops before high order arrays
+
+
+/// First loop we are talking about   is   ======   FOR LOOP 
+
+// for(let i = 0; i < 10 ; i++ ){
+//     console.log(i)
+// }
+
+const names = 
+    ["gossip",
+    "business",
+    "career",
+    "culture",
+    "education",
+    "economy",
+    "entertainment",
+    "entrepreneurship",
+    "fashion",
+    "food",
+    "health",
+    "movies",
+    "music",
+    "online market place",
+    "politics",
+    "products",
+    "real estate",
+    "relationships",
+    "religion",
+    "sex",
+    "social media",
+    "sports",
+    "technology",
+    "travel",]
+
+// console.log(names.length)
+for (let i = 0; i < names.length; i++) {
+    console.log(names[i]);
+}
+
+
+const state = []
+
+
+const movies = [];
