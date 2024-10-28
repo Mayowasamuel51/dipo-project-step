aa9ced7dcd1bf6153b053a7b7718c06d564913f4
mayowasamuel <fpasamuelmayowa51@gmail.com>
2024-10-28 13:27:38 +0100

    new code

---
+++ app/app.js
@@ -0,0 +1,42 @@
+// Aarry 
+const names = ['mayowa', 'dipo', 'shola', 'funke', 'bupe']
+/// array has in built method , 
+//method are the same thing as functions 
+
+names.pop()
+names.unshift('olamide')
+console.log(names)
+
+
+
+// Object 
+// object is all about grouping 
+// key values pairs 
+//Object does not have  in built methods
+/// object you can create you own methods /fucntions
+
+
+const main_group = {
+
+    name: 'shola',
+    age: 21,
+    level: 12,
+    address: 'london road make up',
+    hobbies: ['fighting', 'coding', 'reading'],
+    mymethod: function (x) {
+        return x * x
+    }  //// assigment , explian this code for me 
+}
+
+
+console.log(main_group.mymethod(3))  //explain this code for me 
+
+
+
+// () === it means it a method or a function
+
+
+// remember you can store data in , 
+// Array,
+// Object
+// Mutiple Object in Array   /// make a research on this 
